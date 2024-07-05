cf24f2109ff5db9af1bab721b492d97215fd99d3
Charles Monod-Broca <charles.monod-broca@neuf.fr>
2024-07-05 17:07:46 +0200

    First tests with translations...

---
+++ i18n/cutevariant_en_EN.ts
@@ -0,0 +1,242 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="fr_FR">
+<context>
+    <name>qc.QCoreApplication</name>
+    <message>
+        <location filename="commons.py" line="81"/>
+        <location filename="inspector.py" line="33"/>
+        <location filename="validation_widget.py" line="74"/>
+        <location filename="validation_widget.py" line="114"/>
+        <source>Validation</source>
+        <translation>Validation</translation>
+    </message>
+    <message>
+        <location filename="commons.py" line="84"/>
+        <location filename="commons.py" line="90"/>
+        <location filename="validation_widget.py" line="77"/>
+        <location filename="validation_widget.py" line="83"/>
+        <source>Pas de dossier de configuration trouvé, veuillez en choisir un.</source>
+        <translation>No configuration folder found, please choose one.</translation>
+    </message>
+    <message>
+        <location filename="inspector.py" line="36"/>
+        <source>Filtres de validation</source>
+        <translation>Validation filters</translation>
+    </message>
+    <message>
+        <location filename="query.py" line="203"/>
+        <source>Pas de datalake sélectionné</source>
+        <translation>No datalake selected</translation>
+    </message>
+    <message>
+        <location filename="query.py" line="214"/>
+        <source>Table de validation introuvable</source>
+        <translation>Cannot find validation table</translation>
+    </message>
+    <message>
+        <location filename="query_table_widget.py" line="57"/>
+        <source>Filtrer cette colonne (simple expression)</source>
+        <translation>Filter this column (simple expression)</translation>
+    </message>
+    <message>
+        <location filename="query_table_widget.py" line="68"/>
+        <source>Filter {col_name}</source>
+        <translation>Filtrer {colname}</translation>
+    </message>
+    <message>
+        <location filename="query_table_widget.py" line="69"/>
+        <source>Filtrer une colonne</source>
+        <translation>Filter a column</translation>
+    </message>
+    <message>
+        <location filename="query_table_widget.py" line="70"/>
+        <source>Filtrer</source>
+        <translation>Filter</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="43"/>
+        <source>Nouvelle validation</source>
+        <translation>New validation</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="48"/>
+        <source>Démarrer/Continuer une validation</source>
+        <translation>Start/Resume validation</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="117"/>
+        <source>Veuillez sélectionner une validation à exécuter.</source>
+        <translation>Please select a validation you&apos;d like to execute.</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="190"/>
+        <source>Prochaine étape</source>
+        <translation>Next step</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="193"/>
+        <source>Retour à la sélection des validations</source>
+        <translation>Back to validations selection</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="202"/>
+        <source>Validation terminée</source>
+        <translation>Done validating</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="207"/>
+        <source>Validation terminée.
+Les résultats sont présentés dans la table ci-contre.
+Vous pouvez exporter ces résultats vers Genno en cliquant sur le bouton ci-dessous.</source>
+        <translation>Done validating.
+Results are shown in the table on the right.
+You can export these results to Genno by pressing the button below.</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="208"/>
+        <source>Exporter vers Genno</source>
+        <translation>Export to Genno</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="235"/>
+        <location filename="validation_widget.py" line="248"/>
+        <source>Export</source>
+        <translation>Export</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="238"/>
+        <source>Pas de dossier d&apos;export Genno sélectionné, veuillez en choisir un.</source>
+        <translation>No export folder set for Genno, please select one.</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="242"/>
+        <source>Choisir le dossier d&apos;export Genno</source>
+        <translation>Choose Genno export folder</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="251"/>
+        <source>Pas de dossier d&apos;export Genno sélectionné. Abandon.</source>
+        <translation>No export folder set for Genno. Aborting.</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="274"/>
+        <location filename="validation_widget.py" line="317"/>
+        <location filename="validation_widget.py" line="398"/>
+        <source>Erreur</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="276"/>
+        <source>Le fichier de méthode {method_path} n&apos;existe pas.</source>
+        <translation>Method file {method_path} does not exist.</translation>
+    </message>
+    <message>
+        <location filename="validation_widget.py" line="320"/>
+        <location filename="validation_widget.py" line="401"/>
+        <source>Pas de dossier de configuration sélectionné, abandon.</source>
+        <translation>No config folder set, aborting.</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="19"/>
+        <source>Introduction</source>
+        <translation>Introduction</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="24"/>
+        <source>Ce wizard vous permet de créer une nouvelle validation.</source>
+        <translation>This wizard helps you create a new validation.</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="29"/>
+        <source>Nom de la validation:</source>
+        <translation>Validation name:</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="33"/>
+        <source>Nom de la validation</source>
+        <translation>Validation name</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="103"/>
+        <source>Sélection du run</source>
+        <translation>Run selection</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="106"/>
+        <source>Choisissez le(s) fichier(s) des runs à valider.</source>
+        <translation>Choose run file(s) to validate.</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="110"/>
+        <source>Choisir le(s) run(s)...</source>
+        <translation>Choose run(s)...</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="143"/>
+        <source>Veuillez sélectionner un ou plusieurs runs à valider</source>
+        <translation>Please select one or more runs to validate</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="169"/>
+        <source>Fichiers sélectionnés:
+</source>
+        <translation>Selected files:
+</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="194"/>
+        <source>Sélection des échantillons</source>
+        <translation>Samples selection</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="197"/>
+        <source>Choisissez le(s) échantillon(s) à valider.</source>
+        <translation>Choose the sample(s) you&apos;d like to validate.</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="201"/>
+        <source>Sélectionner les échantillons</source>
+        <translation>Select samples</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="229"/>
+        <source>Echantillons sélectionnés:
+</source>
+        <translation>Selected samples:
+</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="249"/>
+        <source>Sélection de la liste de gènes</source>
+        <translation>Genes list selection</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="252"/>
+        <source>Choisissez la liste de gènes à valider.</source>
+        <translation>Choose the list of genes to validate.</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="256"/>
+        <source>Sélectionner les gènes</source>
+        <translation>Select genes</translation>
+    </message>
+    <message>
+        <location filename="validation_wizard.py" line="296"/>
+        <source>Panel(s): {0}{CR}Gène(s) sélectionné(s):{CR}{1}</source>
+        <translation>Panel(s): {0}{CR}Selected gene(s):{CR}{1}</translation>
+    </message>
+    <message>
+        <location filename="viewer.py" line="35"/>
+        <source>Fichier</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="viewer.py" line="37"/>
+        <location filename="viewer.py" line="77"/>
+        <location filename="viewer.py" line="82"/>
+        <source>Ouvrir un datalake</source>
+        <translation>Open datalake</translation>
+    </message>
+</context>
+</TS>
