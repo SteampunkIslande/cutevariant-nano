6df6ee223606187e54426c5c7dd55cd7b205ad10
Charles Monod-Broca <charles.monod-broca@neuf.fr>
2025-03-14 14:04:02 +0100

    Fixed filters changed (basic)

--- query/query_component.py
+++ query/query_component.py
@@ -531,10 +531,10 @@ class QueryComponent(ap.AppComponent):
             if sender_instance_name == f"{self.instance_name}/order_by":
                 self.order_by = payload["order_by_expression"]
                 self.commit()
-        # if action == "query:filters_changed":
-        #     if sender_instance_name == f"{self.instance_name}/filters":
-        #         self.applied_filter = payload["filter_tree"]
-        #         self.commit()
+        if action == "filters_changed":
+            if sender_instance_name == f"{self.instance_name}/filters":
+                self.applied_filter = payload["filter_tree"]
+                self.commit()
         if action == "selected_fields_changed":
             if sender_instance_name == f"{self.instance_name}/fields":
                 self.set_selected_fields(payload["fields"])
