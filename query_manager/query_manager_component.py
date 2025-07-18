240b0f02db30bacf2dd15b605c2395e3952c0bcf
Charles Monod-Broca <charles.monod-broca@neuf.fr>
2025-07-18 12:17:39 +0200

    Removed unused code

--- query_manager/query_manager_component.py
+++ query_manager/query_manager_component.py
@@ -151,54 +151,6 @@ class QueryManagerComponent(ap.AppComponent):
         self.set_current_query(query_instancename)
         return query
 
-    def new_query(self, query_ui_name: str, data_prep: dict, query_options: dict):
-
-        query_instancename = str(uuid4())
-        query: q.QueryComponent = self.app.instantiate_component(
-            "query",
-            query_instancename,
-        )
-
-        query.set_ui_name(query_ui_name)
-
-        query.setup_query(
-            data_prep,
-            query_options["table_uuid"],
-            query_options["parquet_files"],
-            query_options["gene_names"],
-            query_options["sample_names"],
-        )
-
-        query_item = qg.QStandardItem(query_ui_name)
-        query_item.setData(query_instancename, qc.Qt.ItemDataRole.UserRole)
-        query_item.setEditable(False)
-
-        self.query_model.appendRow(query_item)
-
-        # COMPONENT HOLDERS INSTALLATION
-
-        self.queries_tab_widget.blockSignals(True)
-        self.app.window().add_component_to_window(query, mw.WindowRegion.UPPER)
-
-        self.queries[query_instancename] = query
-        self.query_uiname_toinstancename[query_ui_name] = query_instancename
-
-        self.queries_tab_widget.blockSignals(False)
-
-        # Connect title changed signal to update model only if widget exists
-        if query.widget():
-            query.widget().windowTitleChanged.connect(
-                partial(self.update_query_ui_name, query_ui_name)
-            )
-        else:
-            LOGGER.warning(
-                f"Query {query_instancename} has no widget, cannot connect windowTitleChanged"
-            )
-
-        query.commit()
-        self.set_current_query(query_instancename)
-        return query
-
     def update_query_ui_name(self, old_name: str, new_name: str):
         """Update query name in model and queries dictionary"""
 
